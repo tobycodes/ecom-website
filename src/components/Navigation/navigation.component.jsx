fb0a82b7110da663f005d5b77cfccf867d55e0e5
David Olubusoye <davidolubusoye@gmail.com>
2020-07-24 00:33:32 +0100

    add login functionality

--- src/components/Navigation/navigation.component.jsx
+++ src/components/Navigation/navigation.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './_navigation.styles.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/cart.svg';
+import { auth } from '../../firebase/firebase.utils';
 
 
 
-const Navigation = () => (
+const Navigation = ({ currentUser }) => {
+
+    return (
     <div className='nav'>
         <Link to='/' className='nav__logo'>
             <Logo className='nav__logo-img' />
@@ -17,9 +20,21 @@ const Navigation = () => (
             <NavLink to='/contact' activeClassName='active' className='nav__link'>
                 Contact
             </NavLink>
+            {
+                currentUser ? (
+                    <div onClick={() => auth.signOut()} className='nav__link'>
+                        Sign Out
+                    </div>
+                ) :
+                 <NavLink to='/signin' activeClassName='active' className='nav__link'>
+                    Sign In
+                </NavLink>
+            }
+
+           
         </div>
     </div>
     
-);
+)};
 
 export default Navigation;
