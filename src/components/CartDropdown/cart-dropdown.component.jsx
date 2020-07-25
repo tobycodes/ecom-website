70074f5e6be715517ead3dbc2bf97ee019820554
David Olubusoye <davidolubusoye@gmail.com>
2020-07-25 09:41:47 +0100

    add checkout page

--- src/components/CartDropdown/cart-dropdown.component.jsx
+++ src/components/CartDropdown/cart-dropdown.component.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter, useHistory } from 'react-router-dom';
 
 import './_cart-dropdown.styles.scss';
+
 import CustomButton from '../CustomButton/custom-button.component';
 import CartItem from '../CartItem/cart-item.component';
+
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 
 
-const CartDropdown = ({cartItems}) => (
-    <div className="cart-dropdown">
-        <div className="cart-dropdown__content">
-            {cartItems.map(cartItem => (
-                <CartItem key={cartItem.id} item={cartItem} />
-            ))}
+const CartDropdown = ({cartItems}) => {
+    let history = useHistory();
+    return (
+        <div className="cart-dropdown">
+            <div className="cart-dropdown__content">
+                {cartItems.length ?
+                cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                )) :
+                <span className='empty-message' >Your cart is empty</span>
+            }
+            </div>
+            <div className="cart-dropdown__btn">
+                <CustomButton inverted='true' onClick={() => history.push('/checkout')} >
+                    Go to Checkout
+                </CustomButton>
+            </div>
         </div>
-        <div className="cart-dropdown__btn">
-            <CustomButton inverted='true'>
-                Go to Checkout
-            </CustomButton>
-        </div>
-        
-    </div>
-);
+    )
+};
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
---
+++ src/pages/CheckoutPage/checkout.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './_checkout.styles.scss';
+
+
+const CheckoutPage = () => (
+    <div>
+        CHECKOUT
+    </div>
+);
+
+
+export default CheckoutPage;
